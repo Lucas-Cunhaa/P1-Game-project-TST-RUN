be34eeda2d2339ed54b48067b269a18bc47404c7
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-09-27 17:47:40 -0300

    feat: addded the index_page

--- views/index.py
+++ views/index.py
@@ -1,2 +1,13 @@
 import curses
+import profile_window from window.py
+import game_window from window.py
 
+def home_page(stdscr):
+    profile_window(stdscr)
+    game_window(stdscr) 
+    
+def main(stdscr):
+    home_page(stdscr)
+    start_game(stdscr)
+     
+curses.wrapper(main)
