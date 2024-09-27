ef7b6895ee78f616015667ce676a32d1574ce83f
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-09-27 18:31:44 -0300

    feat: added home_page and window_page recursivity

--- views/index.py
+++ views/index.py
@@ -3,8 +3,19 @@ from window_profile import window_profile
 # from window_game import game_window 
 
 def home_page(stdscr):
-    window_profile(stdscr)
-    # game_window(stdscr) 
+    stdscr.clear()
+
+    stdscr.addstr(0, 0, "GAME: TST-RUN")
+    stdscr.addstr(1, 0, "Press '1' to start the game")
+    stdscr.addstr(2, 0, "Press '2' to change profile")
+
+    stdscr.refresh()
+    KEY_1 = 49
+    KEY_2 = 50
+    while True: 
+        key = stdscr.getch()
+        if key == KEY_2: window_profile(stdscr)
+    # elif key == KEY_1: game_window(stdscr) 
     
 def main(stdscr):
     home_page(stdscr)
