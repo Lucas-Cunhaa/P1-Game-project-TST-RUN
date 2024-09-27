e270c32d9e1df53d14f1c11115e6db4c381f9bde
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-09-27 17:57:49 -0300

    feat: index_page

--- views/index.py
+++ views/index.py
@@ -1,13 +1,13 @@
 import curses
-import profile_window from window_profile.py
-import game_window from window_game.py
+from window_profile import profile_window 
+# from window_game import game_window 
 
 def home_page(stdscr):
     profile_window(stdscr)
-    game_window(stdscr) 
+    # game_window(stdscr) 
     
 def main(stdscr):
     home_page(stdscr)
-    start_game(stdscr)
+    # start_game(stdscr)
      
 curses.wrapper(main)
