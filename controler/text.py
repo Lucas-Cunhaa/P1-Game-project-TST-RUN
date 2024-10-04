7af70dfac203d5edd9cb28ebed5ee36f4c8361fc
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-10-04 15:06:12 -0300

    feat: anme profile adjusted

--- controler/text.py
+++ controler/text.py
@@ -5,17 +5,20 @@ def text(win, mensage, stdscr):
     from views.window_profile import window_profile
     text = ""
     while True:
-        win.addstr(1, len(mensage), text, curses.color_pair(1))
+        win.clear() 
+        win.box()
+        win.addstr(1, 1, mensage, curses.color_pair(1))
+        win.addstr(1, len(mensage) + 1, text, curses.color_pair(1))
         win.refresh()
 
         key = win.getch() 
         
         KEY_ESC = 27
-        KEY_BACKSPACE = 127
+        KEY_BACKSPACE = 8
         KEY_ENTER = 10
         if key == KEY_ESC: 
             return home_page(stdscr)
-        elif key == KEY_BACKSPACE or key == curses.KEY_BACKSPACE:
+        elif key == KEY_BACKSPACE:
             text = text[:-1]
         elif key == KEY_ENTER: 
             break
