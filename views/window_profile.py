075a52d1f33a113732b5a9de447e3e8166294ae9
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-10-04 11:49:02 -0300

    feat: introducing the name profile, but thre is some things to ajust

--- views/window_profile.py
+++ views/window_profile.py
@@ -7,9 +7,17 @@ def window_profile(stdscr, win):
     win.clear()
     win.box()
     
-    win.addstr(1, 1, "Your profile")
-    win.addstr(3, 1, "Change your avatar")
-    win.addstr(4, 1, "Change your name")
+    # Configurações de cor
+    curses.start_color()  # Inicializa o uso de cores
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Texto branco, fundo preto
+
+    mensage = "Your profile: > "
+    win.addstr(1, 1, mensage, curses.color_pair(1))
+    win.refresh() 
+    profile = win.getstr(1, len(mensage)).decode('utf-8')
+    win.addstr(3, 1, f"Name Profile: {profile}", curses.color_pair(1))
+    win.addstr(4, 1, "Change your avatar", curses.color_pair(1))
+    win.addstr(5, 1, "Change your name", curses.color_pair(1))
     win.refresh()
      
     esc = 27
