342cc76443e197187da9d7967d729cf98a700cac
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2024-10-08 15:53:38 -0300

    feat: name_profile at the game_window

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from views.game_view import game_view
 
 def main(stdscr):
     name_profile = "Dalton (DEFAULT)"
-    home_page(stdscr, name_profile, start_game= False)
+    name_profile = home_page(stdscr, name_profile, start_game= False)
     return game_view(stdscr, name_profile)
      
 curses.wrapper(main)
