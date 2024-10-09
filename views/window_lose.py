ddeb9a7c94059a0b3edae23f854970661f5ca814
lucas cunha <lucasgalvao260@gmail.com>
2024-10-09 16:46:02 -0300

    feat: added the vilan photo

--- views/window_lose.py
+++ views/window_lose.py
@@ -6,13 +6,16 @@ def lose_window(stdscr, name_profile):
     from assets.jorge import jorge
 
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    
     stdscr.clear()
 
-    #stdscr.addstr(jorge_start_y, jorge_start_x, jorge)
-    text_window = f"You were impugned by jorge.py {name_profile}"
+    jorge_start_x = 1
+    jorge_start_y = 1
+    stdscr.addstr(jorge_start_y, jorge_start_x, jorge, curses.color_pair(1))
+    stdscr.refresh()
 
+    text_window = f"You were impugned by jorge.py {name_profile}"
     width = len(text_window) + 2
     height = 10  
     max_height, max_width = stdscr.getmaxyx()
@@ -24,8 +27,7 @@ def lose_window(stdscr, name_profile):
     win.box()
 
     x, y = 1, 1
-    jorge_start_x = 5
-    jorge_start_y = 5
+  
 
     delay = 0.1
 
@@ -41,7 +43,7 @@ def lose_window(stdscr, name_profile):
     y_text = height // 2
     win.addstr(y_text, x_text, text)
     win.refresh()
-    
+
     KEY_ENTER = 10
     while True:
         key = win.getch()
