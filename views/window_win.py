cbfcbc36913d5a803d83436087bf1601edf6bae7
lucas cunha <lucasgalvao260@gmail.com>
2024-10-10 08:08:29 -0300

    fix: edited the name of final text

--- views/window_win.py
+++ views/window_win.py
@@ -5,7 +5,6 @@ def win_window(stdscr,name_profile):
     from assets.dalton import dalton
     from controllers.display_ascci_image import display_ascii
     from controllers.display_text import display_text
-
     curses.start_color()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
@@ -15,8 +14,8 @@ def win_window(stdscr,name_profile):
     jorge_y = 1
     display_ascii(stdscr, jorge_y, jorge_x, dalton, 1)
 
-    text_window = f"Hey {name_profile}, You’ve completed all the units for the first semester of Computer Science at UFCG and defated the enemy Jorge.py!"
-    width = len(text_window) + 4
+    final_text = f"Hey {name_profile}, You’ve completed all the units for the first semester of Computer Science at UFCG and defated the enemy Jorge.py!"
+    width = len(final_text) + 4
     height = 10  
     max_height, max_width = stdscr.getmaxyx()
     
@@ -26,9 +25,9 @@ def win_window(stdscr,name_profile):
     win = curses.newwin(height, width, start_y, start_x)
     win.box()
 
-    x, y = 1, 1
+    final_text_x, final_text_y = 1, 1
     delay = 0.06
-    display_text(win, y, x, text_window, delay, 1)
+    display_text(win, final_text_y,  final_text_x, final_text, delay, 1)
     win.refresh()
     text = "Press 'enter' to restart the game"
     x_text = (width // 2) - (len(text) // 2)
