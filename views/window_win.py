60b0a0b8ea51bf511b952494b29b438750629546
lucas cunha <lucasgalvao260@gmail.com>
2024-10-10 13:04:56 -0300

    feat: editing game view points

--- views/window_win.py
+++ views/window_win.py
@@ -6,21 +6,22 @@ def win_window(stdscr,name_profile):
     from controllers.display_ascci_image import display_ascii
     from controllers.display_text import display_text
     from controllers.restart_options import restart_options
-
+    
     curses.start_color()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
     stdscr.clear()
 
     dalton_win_x = 1
     dalton_win_y = 1
     display_ascii(stdscr, dalton_win_y, dalton_win_x, dalton_win, 1)
-
+    
     win_text = f"Hey {name_profile}, you’ve completed all first-semester Computer Science units at UFCG and defeated Jorge.py!"
     width = len(win_text) + 4
     height = 10  
     max_height, max_width = stdscr.getmaxyx()
-    
+
     start_y = (max_height - height) // 2  
     start_x = (max_width - width) // 2
     
