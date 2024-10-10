4ed1d3f037dc42eb039f45a680c86fe29d118a7a
lucas cunha <lucasgalvao260@gmail.com>
2024-10-10 12:21:18 -0300

    fix: changing the lose window variable's name

--- views/window_lose.py
+++ views/window_lose.py
@@ -16,27 +16,27 @@ def lose_window(stdscr, name_profile):
     jorge_y = 1
     display_ascii(stdscr, jorge_y, jorge_x, jorge, 1)
 
-    text_window = f"Hey {name_profile}, you were impugned by jorge.py 😈"
-    width = len(text_window) + 4
+    end_game_text = f"Hey {name_profile}, you were impugned by jorge.py 😈"
+    width = len(end_game_text) + 4
     height = 10  
     max_height, max_width = stdscr.getmaxyx()
-    
     start_y = (max_height - height) // 2  
     start_x = (max_width - width) // 2
     
     win = curses.newwin(height, width, start_y, start_x)
     win.box()
 
-    x, y = 1, 1
+    end_text_x,  end_text_y = 1, 1
     delay = 0.06
-    display_text(win, y, x, text_window, delay, 1)
-    win.refresh()
-    text = "Press 'enter' to restart the game"
-    x_text = (width // 2) - (len(text) // 2)
-    y_text = height // 2
-    win.addstr(y_text, x_text, text)
-    text_2 = "Press 'esc' to finish the game"
-    win.addstr(y_text + 1, x_text + 2, text_2)
+    display_text(win, end_text_y,  end_text_x, end_game_text, delay, 1)
+
+    restart_text = "Press 'enter' to restart the game"
+    restart_text_x  = (width // 2) - (len(restart_text) // 2)
+    restart_text_y = height // 2
+    win.addstr(restart_text_y,  restart_text_x, restart_text)
+
+    finish_text = "Press 'esc' to finish the game"
+    win.addstr(restart_text_y + 1, restart_text_x + 2, finish_text)
 
     win.refresh()
 
